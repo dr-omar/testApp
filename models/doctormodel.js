54f8eb0a65ae39ab497be14554da4fe2f197718e
Omar EL Khatib <54081234+dr-omar@users.noreply.github.com>
2025-01-05 02:59:15 -0600

    Update doctormodel.js

--- models/doctormodel.js
+++ models/doctormodel.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose"
 
 //mongoose.connect('mongodb://localhost:27017/doctor_appointment') // connection of dbms
-mongoose.connect('mongodb+srv://omar:1234@cluster0.2llmf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/doctor_appointment')
+mongoose.connect('mongodb+srv://omar:1234@cluster0.2llmf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/doctor_appointment');
 
 // -------------------- Schema for User Table --------------------//
 const doctor_table = new mongoose.Schema({
